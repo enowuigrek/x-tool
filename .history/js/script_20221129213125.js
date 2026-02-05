ca77fb1e7d7d41df8eff8ece9af1381d4939bfac
enowuigrek <enowuigrek@gmail.com>
2026-02-05 13:26:41 +0100

    Clean code. In generationSku function, if sku has two zeros at the beginning, they are removed, so that the sku with two zeros is accepted in the input and added again before displaying

---
+++ .history/js/script_20221129213125.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const inputLinks = document.querySelector('.input');
+const generationButton = document.querySelector('.generation__button');
+const resultSku = document.querySelector('.result__sku');
+const alert = document.querySelector('.alert');
+const skuFromProductLinkMatcher = new RegExp(/\/p\/(\d*)\-/);
+
+let generateSku = () => {
+
+  const inputText = inputLinks.value;
+  const splitInputTextArray = inputText.split(/\r?\n/);
+  const listSku = [];
+  const alertData = [];
+
+  let resultToCopy = '';
+  let alertDisplay = '';
+
+  for (let pieceOfInputText of splitInputTextArray) {
+
+    //zmiena w number, aby pozbyć się ewentualnych zer z przodu, po zmiana z powrotem na string
+    pieceOfInputText *= 1 += ;
+    pieceOfInputText += '';
+
+    if (pieceOfInputText.length <= 7 && pieceOfInputText.length >= 4) {
+      listSku.push(pieceOfInputText);
+    }
+    // narazie zakomentowane wyciąganie SKU z linku
+    // else if (!null){
+    //   const skuFrominputText = skuFromProductLinkMatcher.exec(pieceOfInputText)[1];
+    //   listSku.push(skuFrominputText);
+    // }
+    else {
+      alertData.push(pieceOfInputText);
+    }
+    console.log(listSku);
+  };
+
+  for (let badSku of alertData) {
+
+    const alertElementSku = '<li>' + badSku + '</li>'
+
+    alertDisplay = alertDisplay + alertElementSku;
+  };
+
+  alert.innerHTML = alertDisplay;
+
+  for (let singleSku of listSku) {
+
+    let addZero = () => {
+      if (parseInt(singleSku) && singleSku.length > 6) {
+        singleSku = '00' + singleSku
+      };
+    };
+
+    addZero();
+
+    const listElementSku = '<li>' + singleSku + '</li>'
+
+    resultToCopy = resultToCopy + listElementSku;
+  };
+
+  resultSku.innerHTML = resultToCopy;
+};
+
+generationButton.addEventListener('click', generateSku);
---
+++ .history/js/script_20221129220526.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const inputLinks = document.querySelector('.input');
+const generationButton = document.querySelector('.generation__button');
+const resultSku = document.querySelector('.result__sku');
+const alert = document.querySelector('.alert');
+const skuFromProductLinkMatcher = new RegExp(/\/p\/(\d*)\-/);
+
+let generateSku = () => {
+
+  const inputText = inputLinks.value;
+  const splitInputTextArray = inputText.split(/\r?\n/);
+
+  const listCorrectSku = [];
+  const listWrongSku = [];
+
+  let resultSku = '';
+  let resultWrongSku= '';
+
+  for (let pieceOfInputText of splitInputTextArray) {
+
+    //zmienia w number, aby pozbyć się ewentualnych zer z przodu,po zmiana z powrotem na string
+    pieceOfInputText *= 1;
+    pieceOfInputText += '';
+
+    if (pieceOfInputText.length <= 7 && pieceOfInputText.length >= 4) {
+
+      let singleSkuToCopy = pieceOfInputText;
+      listCorrectSku.push(singleSkuToCopy);
+    }
+    // narazie zakomentowane wyciąganie SKU z linku
+    // else if (!null){
+    //   const skuFrominputText = skuFromProductLinkMatcher.exec(pieceOfInputText)[1];
+    //   listCorrectSku.push(skuFrominputText);
+    // }
+
+  };
+
+  for (let singleWrongSku of listWrongSku) {
+
+    const listElementWrongSku = '<li>' + singleWrongSku + '</li>'
+    resultWrongSku = resultWrongSku + listElementWrongSku;
+  };
+
+  alert.innerHTML = alertDisplay;
+
+  for (let singleSku of listCorrectSku) {
+
+    let addZero = () => {
+      if (parseInt(singleSku) && singleSku.length > 6) {
+        singleSku = '00' + singleSku
+      };
+    };
+    addZero();
+
+    const listElementSku = '<li>' + singleSku + '</li>'
+    resultSku = resultSku + listElementSku;
+  };
+
+  resultSku.innerHTML = resultSku;
+};
+
+generationButton.addEventListener('click', generateSku);
