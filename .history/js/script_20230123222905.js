4dc9f224ad0fa4fcd6242cdc03826b9d588c4497
enowuigrek <enowuigrek@gmail.com>
2026-02-05 14:33:18 +0100

    clean all, change names

---
+++ .history/js/script_20230123222905.js
@@ -0,0 +1,247 @@
+'use strict';
+
+//INPUTS
+const inputSku = document.getElementById('input_sku');
+const inputLinks = document.getElementById('input_links');
+const inputOrderNumber = document.getElementById('input_order_number');
+
+//BUTTONS
+const generateSkuButton = document.getElementById('generate_sku_button');
+const generateListButton = document.getElementById('generate_list_button');
+const generateSkuFromMessageButton = document.getElementById(
+  'generate_sku_from_message_button'
+);
+const generateLinkFromMessageButton = document.getElementById(
+  'generate_link_from_message_button'
+);
+const generateOrderLinkButton = document.getElementById(
+  'generate_order_link_button'
+);
+
+//RESULTS
+const displaySkuToCopy = document.querySelector('.sku_to_copy');
+const displaySkuToCopyFromLinks = document.querySelector(
+  '.sku_to_copy_from_links'
+);
+const displayOrderLink = document.querySelector('.order_link');
+
+let listSku = [];
+console.log('listSku' + listSku);
+
+let resultList = '';
+console.log('resultList'+ resultList );
+
+let resultLink = '';
+console.log('resultLink' + resultLink);
+
+const clearListSku = () => {
+  resultList = ''
+  resultLink = ''
+  listSku.length = 0;
+};
+const checkSku = (sku) => {
+  //remove zero
+  sku = Number(sku);
+  sku = String(sku);
+  //ad zero if length is 7
+  if (sku.length >= 4 && sku.length <= 6) {
+    return sku;
+  } else if (sku.length == 7) {
+    return '00' + sku;
+  }
+};
+const delateDuplicateAndUndefined = () => {
+  listSku = [...new Set(listSku)];
+
+  if (listSku.indexOf(undefined) != -1) {
+    listSku.splice(listSku.indexOf(undefined), 1);
+  }
+};
+const generateListSku = () => {
+  const input = inputSku.value;
+  const splitInputText = input.split(/\r?\n/);
+
+  for (let pieceOfInputText of splitInputText) {
+    pieceOfInputText = checkSku(pieceOfInputText);
+    listSku.push(pieceOfInputText);
+  }
+};
+const generateSkuFromMessage = () => {
+  clearListSku();
+
+  const input = inputLinks.value;
+  const splitInputText = input.replace(/^\s+|\s+$/g, '').split(/\s+/);
+
+  const extractorSkuFromLink = (xKomLink) => {
+    const skuFromLink = new RegExp(/\/p\/(\d*)/);
+    let skuFromInputLink = skuFromLink.exec(xKomLink)[1];
+    skuFromInputLink = checkSku(skuFromInputLink);
+    listSku.push(skuFromInputLink);
+  };
+
+  const skuFinder = (beforeSku) => {
+    const foundIndex = splitInputText.indexOf(beforeSku);
+    splitInputText[foundIndex] = 'delated';
+    let skuFromText = splitInputText[foundIndex + 1];
+    skuFromText = checkSku(skuFromText);
+    listSku.push(skuFromText);
+  };
+
+  for (let pieceOfInputText of splitInputText) {
+    if (pieceOfInputText.includes('https://www.x-kom.pl/p/'))
+      extractorSkuFromLink(pieceOfInputText);
+
+    if (
+      pieceOfInputText === 'x-kom:' ||
+      pieceOfInputText === 'xkom:' ||
+      pieceOfInputText === 'sku:' ||
+      pieceOfInputText === 'sku'
+    )
+      skuFinder(pieceOfInputText);
+  }
+
+  delateDuplicateAndUndefined(listSku);
+};
+// const generateLinkFromMessage = () => {
+//   const input = inputLinks.value;
+//   const splitInputTextArray = input.replace(/^\s+|\s+$/g, '').split(/\s+/);
+
+//   const skuFinder = (phraseBeforeSku) => {
+//     const foundIndex = splitInputTextArray.indexOf(phraseBeforeSku);
+//     splitInputTextArray[foundIndex] = 'delated';
+//     let skuFromText = splitInputTextArray[foundIndex + 1];
+//     skuFromText = checkSku(skuFromText);
+//     listSku.push(skuFromText);
+//   };
+//   const extractorSkuFromLink = (xKomLink) => {
+//     const skuFromLink = new RegExp(/\/p\/(\d*)/);
+//     let skuFromInputLink = skuFromLink.exec(xKomLink)[1];
+//     skuFromInputLink = checkSku(skuFromInputLink);
+//     listSku.push(skuFromInputLink);
+//   };
+
+//   for (let pieceOfInputText of splitInputTextArray) {
+//     if (pieceOfInputText.includes('https://www.x-kom.pl/p/'))
+//       extractorSkuFromLink(pieceOfInputText);
+
+//     if (
+//       pieceOfInputText === 'x-kom:' ||
+//       pieceOfInputText === 'xkom:' ||
+//       pieceOfInputText === 'sku:' ||
+//       pieceOfInputText === 'sku' ||
+//       pieceOfInputText === '-'
+//     )
+//       skuFinder(pieceOfInputText);
+//   }
+
+//   //usunięcie duplikatów
+//   const listSkuND = [...new Set(listSku)];
+
+//   //jeśli w tablicy jest "undefined, (-1 oznacza, że nie ma takiego indexu)"
+//   if (listSkuND.indexOf(undefined) != -1) {
+//     listSkuND.splice(listSkuND.indexOf(undefined), 1);
+//   }
+
+//   let skuToLink = '';
+//   for (let sku of listSkuND) {
+//     sku = `${sku}%2B`;
+//     skuToLink += sku;
+//   }
+//   skuToLink = skuToLink.slice(0, -3);
+//   resultList = `<a href=https://www.x-kom.pl/szukaj?q=${skuToLink} target="blank"> Lista na stronie </a>`;
+
+//   displaySkuToCopyFromLinks.innerHTML = resultList;
+// };
+
+const displaySkuListToCopy = () => {
+  clearListSku();
+  generateListSku();
+  delateDuplicateAndUndefined();
+
+  for (let sku of listSku) {
+    sku = `${sku} </br>`;
+    resultList += sku;
+  }
+  displaySkuToCopy.innerHTML = resultList;
+};
+const displaySkuListLink = () => {
+  clearListSku();
+  generateListSku();
+  delateDuplicateAndUndefined();
+
+  const linkBegin = 'href=https://www.x-kom.pl/szukaj?q=';
+  let skuToLink = '';
+
+  for (let sku of listSku) {
+    sku = `${sku}%2B`;
+    skuToLink += sku;
+  }
+
+  skuToLink = skuToLink.slice(0, -3);
+
+  if (!skuToLink) {
+    resultLink = ''
+  } else {
+    resultLink = `<a ${linkBegin} ${skuToLink} target="blank"> Lista na stronie </a>`;
+  }
+
+  displaySkuToCopy.innerHTML = resultLink;
+};
+
+const displaySkuListToCopyMessage = () => {
+
+for (let sku of listSku) {
+  sku = `${sku} </br>`;
+  resultList += sku;
+}
+displaySkuToCopyFromLinks.innerHTML = resultList;
+
+const orderLink = () => {
+  const linkBeginXKom =
+    'https://xkom-prod.operations.dynamics.com/?cmp=xkom&mi=SalesTableDetails&SalesId=';
+
+  const linkBeginAlTo =
+    'https://xkom-prod.operations.dynamics.com/?cmp=alto&mi=display:SalesTableDetails&SalesId=';
+
+  const easterEgg = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+
+  let linkBegin = '';
+  let resultOrderLink = '';
+  let orderNumber = inputOrderNumber.value;
+  let orderLink = linkBegin + orderNumber;
+
+  orderNumber = orderNumber.trim();
+
+  if (orderNumber.length === 12) {
+    if (orderNumber.startsWith(7)) {
+      linkBegin = linkBeginXKom;
+    } else if (orderNumber.startsWith(6)) {
+      linkBegin = linkBeginAlTo;
+    } else if (orderNumber.startsWith(9)) {
+      linkBegin = easterEgg;
+    } else {
+      linkBegin = undefined;
+    }
+
+    orderLink = linkBegin + orderNumber;
+
+    if (linkBegin == undefined) {
+      displayOrderLink.innerHTML = 'To nie jest numer zamówienia';
+    } else {
+      const hrefOrderLink = `<a href= ${orderLink} target="blank"> ${orderNumber} </a>`;
+      resultOrderLink += hrefOrderLink;
+      displayOrderLink.innerHTML = resultOrderLink;
+    }
+  } else {
+    displayOrderLink.innerHTML = 'To nie jest numer zamówienia';
+  }
+};
+
+generateSkuButton.addEventListener('click', displaySkuListToCopy);
+generateListButton.addEventListener('click', displaySkuListLink);
+generateSkuFromMessageButton.addEventListener('click', generateSkuFromMessage);
+// generateLinkFromMessageButton.addEventListener(
+//   'click',
+//   generateLinkFromMessage
+// );
+generateOrderLinkButton.addEventListener('click', orderLink);
---
+++ .history/js/script_20230124193117.js
@@ -0,0 +1,201 @@
+'use strict';
+
+//Inputs
+const inputSku = document.getElementById('input_sku');
+const inputMessage = document.getElementById('input_links');
+const inputOrderNumber = document.getElementById('input_order_number');
+
+//Buttons
+const skuListButton = document.getElementById('sku_list_button');
+const skuLinkButton = document.getElementById('sku_link_button');
+const skuListFromMessageButton = document.getElementById('sku_list_from_message_button');
+const skuLinkFromMessageButton = document.getElementById('sku_link_from_message_button');
+const orderLinkButton = document.getElementById('order_link_button');
+
+const clearButton = document.getElementById('clear_button');
+
+
+//Results
+const displaySkuToCopy = document.querySelector('.sku_to_copy');
+const displaySkuToCopyFromLinks = document.querySelector('.sku_to_copy_from_links');
+const displayOrderLink = document.querySelector('.order_link');
+
+let listSkuArr = [];
+let resultList = '';
+let resultLink = '';
+
+const clearlistSku = () => {
+  resultList = '';
+  resultLink = '';
+  listSkuArr.length = 0;
+};
+const delateDuplicateAndUndefined = () => {
+  listSkuArr = [...new Set(listSkuArr)];
+
+  if (listSkuArr.indexOf(undefined) != -1) {
+    listSkuArr.splice(listSkuArr.indexOf(undefined), 1);
+  }
+};
+const checkSku = (sku) => {
+  //remove zero
+  sku = Number(sku);
+  sku = String(sku);
+  //ad zero if length is 7
+  if (sku.length >= 4 && sku.length <= 6) {
+    return sku;
+  } else if (sku.length == 7) {
+    return '00' + sku;
+  }
+};
+
+//Array with sku generators
+const generateInputlistSkuArr = (input) => {
+  const splitInputText = input.split(/\r?\n/);
+
+  for (let pieceOfInputText of splitInputText) {
+    pieceOfInputText = checkSku(pieceOfInputText);
+    listSkuArr.push(pieceOfInputText);
+  }
+};
+const generateSkuFromMessageArr = (input) => {
+  const splitInputText = input.replace(/^\s+|\s+$/g, '').split(/\s+/);
+
+  const extractorSkuFromLink = (xKomLink) => {
+    const skuFromLink = new RegExp(/\/p\/(\d*)/);
+    let skuFromInputLink = skuFromLink.exec(xKomLink)[1];
+    skuFromInputLink = checkSku(skuFromInputLink);
+    listSkuArr.push(skuFromInputLink);
+  };
+
+  const extractorSkuFromText = (beforeSku) => {
+    const foundIndex = splitInputText.indexOf(beforeSku);
+    splitInputText[foundIndex] = 'delated';
+    let skuFromText = splitInputText[foundIndex + 1];
+    skuFromText = checkSku(skuFromText);
+    listSkuArr.push(skuFromText);
+  };
+
+  for (let pieceOfInputText of splitInputText) {
+    if (pieceOfInputText.includes('https://www.x-kom.pl/p/'))
+      extractorSkuFromLink(pieceOfInputText);
+
+    if (
+      pieceOfInputText === 'x-kom:' ||
+      pieceOfInputText === 'xkom:' ||
+      pieceOfInputText === 'sku:' ||
+      pieceOfInputText === 'sku'
+    )
+      extractorSkuFromText(pieceOfInputText);
+  }
+};
+
+//List to copy and link to list generators
+const listToCopy = (arr) => {
+  for (let sku of arr) {
+    sku = `${sku} </br>`;
+    resultList += sku;
+  }
+};
+const linkToList = (arr) => {
+  const linkBegin = 'href=https://www.x-kom.pl/szukaj?q=';
+  let skuToLink = '';
+
+  for (let sku of arr) {
+    sku = `${sku}%2B`;
+    skuToLink += sku;
+  }
+
+  skuToLink = skuToLink.slice(0, -3);
+
+  if (!skuToLink) {
+    resultLink = '';
+  } else {
+    resultLink = `<a ${linkBegin}${skuToLink} target="_blank"> Lista na stronie </a>`;
+  }
+};
+
+const displaySkuListToCopy = () => {
+  clearlistSku();
+
+  generateInputlistSkuArr(inputSku.value);
+
+  delateDuplicateAndUndefined();
+
+  listToCopy(listSkuArr);
+
+  displaySkuToCopy.innerHTML = resultList;
+};
+const displaySkuListLink = () => {
+  clearlistSku();
+  generateInputlistSkuArr(inputSku.value);
+  delateDuplicateAndUndefined();
+  linkToList(listSkuArr);
+  displaySkuToCopy.innerHTML = resultLink;
+};
+const displaySkuListToCopyMessage = () => {
+  clearlistSku();
+  generateSkuFromMessageArr(inputMessage.value);
+  delateDuplicateAndUndefined();
+  listToCopy(listSkuArr);
+  displaySkuToCopyFromLinks.innerHTML = resultList;
+};
+const displaySkuListLinkMessage = () => {
+  clearlistSku();
+  generateSkuFromMessageArr(inputMessage.value);
+  delateDuplicateAndUndefined();
+  linkToList(listSkuArr);
+  displaySkuToCopyFromLinks.innerHTML = resultLink;
+};
+
+const orderLink = () => {
+  const linkBeginXKom =
+    'https://xkom-prod.operations.dynamics.com/?cmp=xkom&mi=SalesTableDetails&SalesId=';
+
+  const linkBeginAlTo =
+    'https://xkom-prod.operations.dynamics.com/?cmp=alto&mi=display:SalesTableDetails&SalesId=';
+
+  const xKomShop = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+
+  let linkBegin = '';
+  let resultOrderLink = '';
+  let orderNumber = inputOrderNumber.value;
+  let orderLink = linkBegin + orderNumber;
+
+  orderNumber = orderNumber.trim();
+
+  if (orderNumber.length === 12) {
+    if (orderNumber.startsWith(7)) {
+      linkBegin = linkBeginXKom;
+    } else if (orderNumber.startsWith(6)) {
+      linkBegin = linkBeginAlTo;
+    } else if (orderNumber.startsWith(9)) {
+      linkBegin = xKomShop;
+    } else {
+      linkBegin = undefined;
+    }
+
+    orderLink = linkBegin + orderNumber;
+
+    if (linkBegin == undefined) {
+      displayOrderLink.innerHTML = 'To nie jest numer zamówienia';
+    } else {
+      const hrefOrderLink = `<a href= ${orderLink} target="_blank"> ${orderNumber} </a>`;
+      resultOrderLink += hrefOrderLink;
+      displayOrderLink.innerHTML = resultOrderLink;
+    }
+  } else {
+    displayOrderLink.innerHTML = 'To nie jest numer zamówienia';
+  }
+};
+
+const clear = (input) => {
+  input.value = '';
+}
+
+skuListButton.addEventListener('click', displaySkuListToCopy);
+skuLinkButton.addEventListener('click', displaySkuListLink);
+skuListFromMessageButton.addEventListener('click', displaySkuListToCopyMessage);
+skuLinkFromMessageButton.addEventListener('click', displaySkuListLinkMessage);
+orderLinkButton.addEventListener('click', orderLink);
+
+clearButton.addEventListener('click', clear(inputSku ));
